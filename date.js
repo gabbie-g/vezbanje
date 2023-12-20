f99cf2e9fccfdd8395738355c2fc828c5c2992ad
gabriela <gabriela.gradinaru@hotmail.com>
2023-12-20 14:42:58 +0100

    version-2

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
 var d = new Date();
-document.getElementById("date-time").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear() + " " +  
-    d.getHours() + ":" +  d.getMinutes();
+document.getElementById("date").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
 
+document.getElementById("time").innerHTML = d.getHours() + ":" +  d.getMinutes();
