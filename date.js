afbc0c1527e045b005a40d449e3406e98a12e5a0
gabriela <gabriela.gradinaru@hotmail.com>
2023-12-20 23:11:41 +0100

    minutes correction

--- date.js
+++ date.js
@@ -1,4 +1,8 @@
 var d = new Date();
 document.getElementById("date").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
 
-document.getElementById("time").innerHTML = d.getHours() + ":" +  d.getMinutes();
+if(d.getMinutes() < 10){
+    document.getElementById("time").innerHTML = d.getHours() + ":" + "0" + d.getMinutes();
+}else{
+    document.getElementById("time").innerHTML = d.getHours() + ":" +  d.getMinutes();
+}
