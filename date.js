cb0cedb206bdd750e5ed2ca0c80f484eb163407c
gabriela <gabriela.gradinaru@hotmail.com>
2023-12-20 23:33:32 +0100

    date correction

--- date.js
+++ date.js
@@ -1,5 +1,13 @@
 var d = new Date();
-document.getElementById("date").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+if(d.getDate() < 10){
+    document.getElementById("date").innerHTML = "0" + d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+}else if(d.getMonth() < 9){
+    document.getElementById("date").innerHTML = d.getDate() + ".0" + (d.getMonth() + 1) + "." + d.getFullYear();
+} else if(d.getDate() < 10 && d.getMonth() < 9){
+    document.getElementById("date").innerHTML = d.getDate() + ".0" + (d.getMonth() + 1) + "." + d.getFullYear();
+} else {
+    document.getElementById("date").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+}
 
 if(d.getMinutes() < 10){
     document.getElementById("time").innerHTML = d.getHours() + ":" + "0" + d.getMinutes();
