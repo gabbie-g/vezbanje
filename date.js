e6a649eb4fa2dd58e6c68be5d31f77b2afd1f76c
gabbie-g <88021584+gabbie-g@users.noreply.github.com>
2023-12-21 09:00:44 +0100

    Update date.js

--- date.js
+++ date.js
@@ -9,8 +9,12 @@ if(d.getDate() < 10){
     document.getElementById("date").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
 }
 
-if(d.getMinutes() < 10){
+if(d.getHours() < 10){
+    document.getElementById("time").innerHTML = "0" + d.getHours() + ":" + d.getMinutes();
+}else if(d.getMinutes() < 10){
     document.getElementById("time").innerHTML = d.getHours() + ":" + "0" + d.getMinutes();
+}else if(d.getHours() < 10 && d.getMinutes() < 10){
+    document.getElementById("time").innerHTML =  "0" + d.getHours() + ":" + "0" + d.getMinutes();
 }else{
     document.getElementById("time").innerHTML = d.getHours() + ":" +  d.getMinutes();
 }
