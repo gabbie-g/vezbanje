d36f08d26e41166f7ced22487256b21b84f84231
gabriela <gabriela.gradinaru@hotmail.com>
2023-12-21 09:09:55 +0100

    additional date corrections

--- date.js
+++ date.js
@@ -1,20 +1,21 @@
 var d = new Date();
-if(d.getDate() < 10){
+
+if(d.getDate() < 10 && d.getMonth() < 9){
+    document.getElementById("date").innerHTML = d.getDate() + ".0" + (d.getMonth() + 1) + "." + d.getFullYear();
+}else if(d.getDate() < 10){
     document.getElementById("date").innerHTML = "0" + d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
 }else if(d.getMonth() < 9){
     document.getElementById("date").innerHTML = d.getDate() + ".0" + (d.getMonth() + 1) + "." + d.getFullYear();
-} else if(d.getDate() < 10 && d.getMonth() < 9){
-    document.getElementById("date").innerHTML = d.getDate() + ".0" + (d.getMonth() + 1) + "." + d.getFullYear();
-} else {
+}else{
     document.getElementById("date").innerHTML = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
 }
 
-if(d.getHours() < 10){
+if(d.getHours() < 10 && d.getMinutes() < 10){
+    document.getElementById("time").innerHTML =  "0" + d.getHours() + ":" + "0" + d.getMinutes();
+}else if(d.getHours() < 10){
     document.getElementById("time").innerHTML = "0" + d.getHours() + ":" + d.getMinutes();
 }else if(d.getMinutes() < 10){
     document.getElementById("time").innerHTML = d.getHours() + ":" + "0" + d.getMinutes();
-}else if(d.getHours() < 10 && d.getMinutes() < 10){
-    document.getElementById("time").innerHTML =  "0" + d.getHours() + ":" + "0" + d.getMinutes();
 }else{
     document.getElementById("time").innerHTML = d.getHours() + ":" +  d.getMinutes();
 }
